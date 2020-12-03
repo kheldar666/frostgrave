11f2962803b740b3856b461a9e9f8171dd498b84
Martin Papy <martin.papy@gmail.com>
2020-12-03 10:53:12 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# frostgrave
+# Frostgrave Campaign Manager
+
+The goal of this project is to have an easy way for friends and foes to manage their Frostgrave campaigns.
+
---
+++ src/test/java/org/libermundi/frostgrave/FrostgraveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.libermundi.frostgrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostgraveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
